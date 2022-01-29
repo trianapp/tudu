d3eebe192b5cd861771446599683d71f4caa7032
triandamai <triannurizkillah@gmail.com>
2022-01-29 12:37:52 +0700

    chore: add routeName

--- app/src/main/java/app/trian/tudu/common/Routes.kt
+++ app/src/main/java/app/trian/tudu/common/Routes.kt
@@ -1,15 +1,21 @@
 package app.trian.tudu.common
 
+/**
+ * Define Each routes
+ * author Trian Damai
+ * created_at 28/01/22 - 10.29
+ * site https://trian.app
+ */
 object Routes {
-    val SPLASH = "SPALSH"
-    val ONBOARD = "ONBOARD"
-    val LOGIN = "LOGIN"
-    val REGISTER = "REGISTER"
-    val DASHBOARD = "DASHBBOARD"
-    val CATEGORY = "CATEGORY"
+    const val SPLASH = "SPLASH"
+    const val ONBOARD = "ONBOARD"
+    const val LOGIN = "LOGIN"
+    const val REGISTER = "REGISTER"
+    const val DASHBOARD = "DASHBOARD"
+    const val CATEGORY = "CATEGORY"
     object Dashboard{
-        val HOME = "DASHBOARD_HOME"
-        val CALENDER = "DASHBOARD_CALENDER"
-        val PROFILE = "DASHBOARD_PROFILE"
+        const val HOME = "DASHBOARD_HOME"
+        const val CALENDER = "DASHBOARD_CALENDER"
+        const val PROFILE = "DASHBOARD_PROFILE"
     }
 }
