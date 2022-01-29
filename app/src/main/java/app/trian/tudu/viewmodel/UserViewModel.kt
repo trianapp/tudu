cd07a426e8fb3e0bec4a7767bada079f8e564e51
triandamai <triannurizkillah@gmail.com>
2022-01-29 12:38:35 +0700

    feat: UserViewModel.kt

--- app/src/main/java/app/trian/tudu/viewmodel/UserViewModel.kt
+++ app/src/main/java/app/trian/tudu/viewmodel/UserViewModel.kt
@@ -2,18 +2,66 @@ package app.trian.tudu.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import app.trian.tudu.data.repository.design.UserRepository
+import app.trian.tudu.domain.DataState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+/**
+ * Database
+ * author Trian Damai
+ * created_at 28/01/22 - 10.02
+ * site https://trian.app
+ */
 @HiltViewModel
-class UserViewModel @Inject constructor(
-    private val userRepository: UserRepository
-):ViewModel() {
-    private val _counter = MutableLiveData<Int>(0)
-    val counter get() = _counter
+class UserViewModel @Inject constructor():ViewModel() {
+    @Inject  lateinit var userRepository: UserRepository
 
-    fun increment(){
-        _counter.value = (_counter.value?.plus(1))
+    /**
+     * cek id user already logged in ?
+     * we user firebase auth
+     * */
+    fun userAlreadyLogin(callback:(isLoggedIn:Boolean)->Unit)=viewModelScope.launch {
+        delay(1000)
+        userRepository.checkIsUserLogin()
+            .collect {
+               callback(it)
+            }
+    }
+
+    fun loggedInWithEmailAndPassword(
+        email:String,
+        password:String,
+        callback: (success: Boolean,message:String) -> Unit
+    )=viewModelScope.launch {
+        userRepository.loginBasic(email, password).collect{
+            result->
+            when(result){
+                DataState.LOADING -> {}
+                is DataState.onData -> {callback(true,"Sukses")}
+                is DataState.onFailure -> {callback(false,result.message)}
+            }
+        }
+    }
+
+    fun registerWithEmailAndPassword(
+        email:String,
+        password:String,
+        callback: (success: Boolean,message:String) -> Unit
+    )=viewModelScope.launch {
+        userRepository.registerBasic(email, password).collect {
+            result->
+            when(result){
+                DataState.LOADING -> {}
+                is DataState.onData -> {callback(true,"sukses")}
+                is DataState.onFailure -> {callback(false,result.message)}
+            }
+        }
     }
 }
+
+
