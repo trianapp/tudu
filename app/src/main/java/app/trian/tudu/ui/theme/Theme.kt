cd8a6b0d45887cf97eed21d225d3baea5d4d7f09
triandamai <triannurizkillah@gmail.com>
2022-01-30 12:18:59 +0700

    chore: disable dynamic schema color

--- app/src/main/java/app/trian/tudu/ui/theme/Theme.kt
+++ app/src/main/java/app/trian/tudu/ui/theme/Theme.kt
@@ -44,7 +44,7 @@ private val DarkColorScheme = darkColorScheme(
 fun TuduTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
