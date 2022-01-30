bd96a43bed2b1b5198fc956eed63bca523f9e202
triandamai <triannurizkillah@gmail.com>
2022-01-30 12:20:10 +0700

    chore: add appbar

--- app/src/main/java/app/trian/tudu/ui/component/Appbar.kt
+++ app/src/main/java/app/trian/tudu/ui/component/Appbar.kt
@@ -4,10 +4,14 @@ import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material3.IconToggleButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import app.trian.tudu.ui.theme.TuduTheme
 import compose.icons.Octicons
+import compose.icons.octicons.ArrowLeft16
 import compose.icons.octicons.Share24
 
 /**
@@ -32,6 +36,27 @@ fun AppbarHome(
                   }
         },
         navigationIcon = {}
+    )
+}
+@Composable
+fun AppbarAuth(
+    modifier: Modifier=Modifier,
+    onBackPressed:()->Unit={}
+){
+    TopAppBar(
+        backgroundColor = MaterialTheme.colorScheme.background,
+        elevation = 0.dp,
+        navigationIcon = {
+            IconToggleButton(checked = false, onCheckedChange = {
+                onBackPressed()
+            }) {
+                androidx.compose.material3.Icon(
+                    imageVector = Octicons.ArrowLeft16,
+                    contentDescription = ""
+                )
+            }
+        },
+        title = {}
     )
 }
 
