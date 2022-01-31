b867c05500c38c30337f6eb758a9aca8fb461936
triandamai <triannurizkillah@gmail.com>
2022-01-31 23:18:22 +0700

    feat: finishing page category management

---
+++ app/src/main/java/app/trian/tudu/ui/component/dialog/DialogFormCategory.kt
@@ -0,0 +1,157 @@
+package app.trian.tudu.ui.component.dialog
+
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import app.trian.tudu.R
+import app.trian.tudu.ui.theme.InactiveText
+import app.trian.tudu.ui.theme.TuduTheme
+
+/**
+ * Dialog Form Category
+ * author Trian Damai
+ * created_at 29/01/22 - 20.00
+ * site https://trian.app
+ */
+@Composable
+fun DialogFormCategory(
+    show:Boolean=false,
+    onHide:()->Unit,
+    onSubmit:(value:String)->Unit
+) {
+    if(show) {
+        Dialog(onDismissRequest = {
+            onHide()
+        }) {
+            ScreenDialogFormCategory(
+                onSubmit = onSubmit,
+                onHide = onHide
+            )
+        }
+    }
+}
+
+@Composable
+fun ScreenDialogFormCategory(
+    modifier: Modifier=Modifier,
+    onHide: () -> Unit={},
+    onSubmit: (value:String) -> Unit
+) {
+    val ctx = LocalContext.current
+    var categoryName by remember {
+        mutableStateOf("")
+    }
+    fun submit(){
+        if(categoryName.isBlank()){
+            Toast.makeText(ctx,ctx.getString(R.string.blank_validation,"Category"),Toast.LENGTH_LONG).show()
+        }else{
+            onSubmit(categoryName)
+            onHide()
+        }
+    }
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(
+                topStart = 10.dp,
+                topEnd = 10.dp,
+                bottomStart = 10.dp,
+                bottomEnd = 10.dp
+            ))
+            .background(MaterialTheme.colorScheme.background)
+            .padding(horizontal = 16.dp, vertical = 16.dp)
+    ) {
+        Column {
+            Text(
+                text = stringResource(R.string.title_create_new_cateogory),
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+            Spacer(modifier = modifier.height(16.dp))
+            Box {
+                TextField(
+                    modifier=modifier.fillMaxWidth().align(Alignment.TopCenter),
+                    value = categoryName,
+                    placeholder = {
+                        Text(text = stringResource(R.string.placeholder_input_category))
+                    },
+                    onValueChange ={
+                        categoryName = it
+                    },
+                    colors = TextFieldDefaults.textFieldColors(
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent
+                    ),
+                    textStyle = TextStyle(
+                        color = Color.Black,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    ),
+                    shape = RoundedCornerShape(
+                        topStart = 10.dp,
+                        topEnd = 10.dp,
+                        bottomStart = 10.dp,
+                        bottomEnd = 10.dp
+                    )
+                )
+                Text(
+                    text = "0/50",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.BottomEnd)
+                        .padding(
+                            top = 4.dp,
+                            end = 10.dp,
+                            bottom = 4.dp
+                        ),
+                    textAlign = TextAlign.End,
+                    color = InactiveText
+                )
+            }
+
+            Row(
+                modifier=modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                TextButton(onClick = {
+                    onHide()
+                }) {
+                    Text(text = stringResource(R.string.btn_cancel))
+                }
+                TextButton(onClick = { submit() }) {
+                    Text(text = stringResource(R.string.btn_save))
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewScreenFromCategory(){
+    TuduTheme {
+        ScreenDialogFormCategory{}
+    }
+}
--- app/src/main/java/app/trian/tudu/ui/pages/category/PagesCategoryManagement.kt
+++ app/src/main/java/app/trian/tudu/ui/pages/category/PagesCategoryManagement.kt
@@ -1,23 +1,33 @@
 package app.trian.tudu.ui.pages.category
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.IconToggleButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import app.trian.tudu.ui.component.DialogFormCategory
+import app.trian.tudu.R
+import app.trian.tudu.ui.component.dialog.DialogFormCategory
 import app.trian.tudu.ui.component.ItemAddCategory
 import app.trian.tudu.ui.component.ItemCategory
+import app.trian.tudu.ui.theme.Inactivebackground
 import app.trian.tudu.ui.theme.TuduTheme
 import app.trian.tudu.viewmodel.TaskViewModel
+import compose.icons.Octicons
+import compose.icons.octicons.ArrowLeft16
 import logcat.logcat
 
 @Composable
@@ -45,20 +55,40 @@ fun PagesCategoryManagement(
         }
     )
 
-    Scaffold {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                backgroundColor = MaterialTheme.colorScheme.background,
+                elevation = 0.dp,
+                navigationIcon = {
+                    IconToggleButton(checked = false, onCheckedChange = {
+                        router.popBackStack()
+                    }) {
+                        Icon(
+                            imageVector = Octicons.ArrowLeft16,
+                            contentDescription = ""
+                        )
+                    }
+                },
+                title = {
+                    Text(text = stringResource(R.string.title_category_management))
+                }
+            )
+        }
+    ) {
         LazyColumn(
             content = {
                 item {
                     Row (
                         modifier= modifier
                             .fillMaxWidth()
-                            .padding(
-                                horizontal = 16.dp,
-                                vertical = 6.dp
-                            ),
+                            .background(Inactivebackground)
+                            .padding(vertical = 8.dp),
                         horizontalArrangement = Arrangement.Center
                     ){
-                       Text(text = "Category display on homepage")
+                       Text(
+                           text = stringResource(R.string.subtitle_category_management)
+                       )
                     }
                 }
                 items(listCategory){
@@ -67,7 +97,6 @@ fun PagesCategoryManagement(
                 }
                 item {
                     ItemAddCategory {
-                        logcat("harus pokonya") {"iaiia"  }
                         shouldShowDialogFormCategory=true
                     }
                 }
