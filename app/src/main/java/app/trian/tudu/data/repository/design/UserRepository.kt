365cff6a8461b2f8d1711bda3d77cd64ee1e2338
triandamai <triannurizkillah@gmail.com>
2022-01-31 23:19:51 +0700

    feat: add calculate task ,current user

--- app/src/main/java/app/trian/tudu/data/repository/design/UserRepository.kt
+++ app/src/main/java/app/trian/tudu/data/repository/design/UserRepository.kt
@@ -8,6 +8,8 @@ interface UserRepository {
 
     suspend fun checkIsUserLogin():Flow<Boolean>
 
+    suspend fun getCurrentUser():Flow<FirebaseUser?>
+
     suspend fun loginBasic(email:String,password:String):Flow<DataState<FirebaseUser>>
     suspend fun registerBasic(username:String,email:String,password:String):Flow<DataState<FirebaseUser>>
 
--- app/src/main/java/app/trian/tudu/ui/pages/dashbboard/PageProfile.kt
+++ app/src/main/java/app/trian/tudu/ui/pages/dashbboard/PageProfile.kt
@@ -1,21 +1,66 @@
 package app.trian.tudu.ui.pages.dashbboard
 
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ModalBottomSheetValue
-import androidx.compose.material.rememberModalBottomSheetState
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.GenericShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import app.trian.tudu.R
+import app.trian.tudu.ui.component.customShape.CurveShape
 import app.trian.tudu.ui.component.task.BottomSheetInputNewTask
+import app.trian.tudu.ui.theme.TuduTheme
+import app.trian.tudu.viewmodel.TaskViewModel
+import app.trian.tudu.viewmodel.UserViewModel
+import compose.icons.Octicons
+import compose.icons.octicons.ArrowLeft24
+import compose.icons.octicons.Gear16
 
 @ExperimentalMaterialApi
 @Composable
 fun PageProfile(
+    modifier: Modifier=Modifier,
     router: NavHostController
 ){
+    val userViewModel = hiltViewModel<UserViewModel>()
+    val taskViewModel = hiltViewModel<TaskViewModel>()
+    val currentUser by userViewModel.currentUser.observeAsState()
+
+    val allTaskCount by taskViewModel.allTaskCount.observeAsState(initial = 0)
+    val completeTaskCount by taskViewModel.completedTaskCount.observeAsState(initial = 0)
+    val unCompleteTaskCount by taskViewModel.unCompleteTaskCount.observeAsState(initial = 0)
+
     val modalBottomSheetState = rememberModalBottomSheetState(
         initialValue = ModalBottomSheetValue.Hidden,
         skipHalfExpanded = false,
     )
+
+    LaunchedEffect(key1 = Unit, block = {
+        userViewModel.getCurrentUser()
+        taskViewModel.calculateTaskCount()
+    })
     BasePagesDashboard(
         router = router,
         sheetContent={
@@ -23,6 +68,143 @@ fun PageProfile(
         },
         modalBottomSheetState=modalBottomSheetState
     ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    backgroundColor = MaterialTheme.colorScheme.primary,
+                    elevation = 0.dp,
+                    navigationIcon = {
+                        IconToggleButton(checked = false, onCheckedChange = {
+                            router.popBackStack()
+                        }) {
+                            Icon(
+                                imageVector = Octicons.ArrowLeft24,
+                                contentDescription = "",
+                                tint = MaterialTheme.colorScheme.onPrimary
+                            )
+                        }
+                    },
+                    title = {
+                            Text(
+                                stringResource(R.string.title_page_profile),
+                                style = TextStyle(
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    color = MaterialTheme.colorScheme.onPrimary
+                                )
+                            )
+                    },
+                    actions = {
+                        IconToggleButton(checked = false, onCheckedChange = {}) {
+                            Icon(
+                                imageVector = Octicons.Gear16,
+                                contentDescription = "",
+                                tint = MaterialTheme.colorScheme.onPrimary
+                            )
+                        }
+                    }
+                )
+            }
+        ) {
 
+            Box {
+                Box (
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .height(80.dp)
+                        .clip(
+                            RoundedCornerShape(
+                                bottomStartPercent = 100,
+                                bottomEndPercent = 100
+                            )
+                        )
+                        .background(MaterialTheme.colorScheme.primary)
+                ){
+
+                }
+                Card(
+                    modifier=modifier.padding(
+                        horizontal = 30.dp,
+                        vertical = 20.dp
+                    )
+                ) {
+                    Column(
+                        modifier=modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Spacer(modifier = modifier.height(20.dp))
+                        Image(
+
+                            modifier= modifier
+                                .size(70.dp)
+                                .clip(CircleShape),
+                            painter = painterResource(id = R.drawable.ic_onboard),
+                            contentDescription = "",
+                        )
+
+                        Column(
+                            modifier=modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Spacer(modifier = modifier.height(30.dp))
+                            Text(currentUser?.displayName ?: stringResource(R.string.placeholder_unknown))
+                            Text(currentUser?.email ?: stringResource(id = R.string.placeholder_unknown))
+                            Spacer(modifier = modifier.height(30.dp))
+                        }
+                        Column(
+                            modifier = modifier.fillMaxWidth(),
+                        ) {
+                            Divider()
+                            Spacer(modifier = modifier.height(10.dp))
+                            Row(
+                                modifier = modifier
+                                    .fillMaxWidth()
+                                    .padding(
+                                        horizontal = 30.dp
+                                    ),
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Column(
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.Center
+                                ) {
+                                    Text(text = "$allTaskCount")
+                                    Text(text = stringResource(R.string.label_total_task))
+                                }
+                                Column(
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.Center
+                                ) {
+                                    Text(text = "$completeTaskCount")
+                                    Text(text = stringResource(R.string.label_complete_task))
+                                }
+                                Column(
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.Center
+                                ) {
+                                    Text(text = "$unCompleteTaskCount")
+                                    Text(text = stringResource(R.string.label_uncomplete_task))
+                                }
+                            }
+                        }
+                        Spacer(modifier = modifier.height(30.dp))
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+@ExperimentalMaterialApi
+@Preview
+@Composable
+fun PreviewPageProfile() {
+    TuduTheme {
+        PageProfile(router = rememberNavController())
     }
 }
