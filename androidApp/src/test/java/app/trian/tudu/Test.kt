d2e91c352899aa01bd0f928c7ea66d61c3b88a04
IBM Trian Damai <triannurizkillah@gmail.com>
2023-03-25 20:17:01 +0700

    finishing

--- androidApp/src/test/java/app/trian/tudu/Test.kt
+++ androidApp/src/test/java/app/trian/tudu/Test.kt
@@ -1,13 +1,12 @@
 package app.trian.tudu
 
-import app.trian.tudu.base.extensions.getFirstDays
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import java.time.LocalDate
 
 class Test {
 
     @Test
     fun `get Days of week`(){
+        assertEquals("a","a")
     }
 }
