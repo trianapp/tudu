b10bac85b42cf1c07ace1492060a5087245df35c
IBM Trian Damai <triannurizkillah@gmail.com>
2023-03-26 16:17:01 +0700

    V2.0.202303261616

--- androidApp/src/main/java/app/trian/tudu/feature/dashboard/home/Page.kt
+++ androidApp/src/main/java/app/trian/tudu/feature/dashboard/home/Page.kt
@@ -23,7 +23,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import app.trian.tudu.ApplicationState
